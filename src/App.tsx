2d82c8b7b5eb752df2519cef6b3a4399cade9781
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2023-11-18 19:36:57 +0100

    add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,19 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <Router>
-        <div className="app-bar">
-    <div className="logo">
-        <a href="#">MyApp</a>
-    </div>
-    <nav>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-    </nav>
-</div>
+      <div className="app-bar">
+        <div className="logo">
+            <a href="#">MyGlamSite 😘</a>
+        </div>
+        <nav className="mobile-nav">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#glitter">Glitter</a></li>
+                <li><a href="#sparkle">Sparkle</a></li>
+                <li><a href="#fun">Fun</a></li>
+            </ul>
+        </nav>
+      </div>
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/horoscope" Component={Horoscope} />
